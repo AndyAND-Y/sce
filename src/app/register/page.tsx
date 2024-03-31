ec3f5e0892086dbc034654374cad5516508250a3
Andrei Slatinaru <andreislatinaru@gmail.com>
2024-03-31 17:53:55 +0100

    login, register protected

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,13 @@
 import RegisterPage from "@/components/register/RegisterPage";
+import getCurrentUser from "@/data/getCurrentUser";
+import { redirect } from "next/navigation";
 
-export default function Register() {
+export default async function Register() {
+
+    const currentUser = await getCurrentUser();
+    if (currentUser) {
+        redirect('/');
+    }
+
     return <RegisterPage />
 }
--- src/app/support/login/page.tsx
+++ src/app/support/login/page.tsx
@@ -1,6 +1,14 @@
 import LoginPage from "@/components/login/LoginPage";
+import getCurrentUser from "@/data/getCurrentUser";
+import { redirect } from "next/navigation";
 
 
-export default function SupportLogin() {
+export default async function SupportLogin() {
+
+    const currentUser = await getCurrentUser();
+    if (currentUser) {
+        redirect('/');
+    }
+
     return (<LoginPage title={"Support Login"} redirectLink={"/support/"} support />)
 }
